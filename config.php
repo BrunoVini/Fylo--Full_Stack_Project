50495cbf6e97a83853a64ae40c9b122ac136fd33
Bruno Souza <66313781+BrunoVini@users.noreply.github.com>
2022-01-23 11:57:12 -0300

    Update config.php

--- config.php
+++ config.php
@@ -12,7 +12,7 @@
 
     spl_autoload_register($autoload);
 
-    define('INCLUDE_PATH','http://192.168.0.106/Udemy/STACK/Back-end/PHP/Crud/');
+    define('INCLUDE_PATH','http://localhost/');
     define('INCLUDE_PATH_PAINEL', INCLUDE_PATH.'painel/');
     define('BASE_DIR_PAINEL',__DIR__.'/painel');
 
