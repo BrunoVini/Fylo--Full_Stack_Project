e2ed32cde05efe84b3bebad4595c5a4e5327567c
Bruno Souza <66313781+BrunoVini@users.noreply.github.com>
2022-02-25 15:05:31 -0300

    Update config.php

--- config.php
+++ config.php
@@ -12,7 +12,7 @@
 
     spl_autoload_register($autoload);
 
-    define('INCLUDE_PATH','http://localhost/');
+    define('INCLUDE_PATH','http://localhost/SEULOCAL');
     define('INCLUDE_PATH_PAINEL', INCLUDE_PATH.'painel/');
     define('BASE_DIR_PAINEL',__DIR__.'/painel');
 
